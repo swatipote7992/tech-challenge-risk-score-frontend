601a92bcb5aa6735107d195ea15b5d2266776b36
Joe Barnett <joe.barnett@elliptic.co>
2023-04-18 12:24:56 +0100

    update readme and format code

--- src/calculateRiskScore.ts
+++ src/calculateRiskScore.ts
@@ -1,13 +1,10 @@
-import transactions from '../data/transactions'
+import transactions from "../data/transactions";
 
 const calculateRiskScore = (transactionHash): number => {
-    const transactionData = transactions[transactionHash]
+  const transactionData = transactions[transactionHash];
 
-    if (!transactionData) {
-        throw new Error('404 transaction not found')
-    }
-
-    return Math.random() * 10
-}
+  // TODO - compute the correct risk score - this is obviously wrong!
+  return Math.random() * 10;
+};
 
 export default calculateRiskScore;
--- types/rule.ts
+++ types/rule.ts
@@ -1,25 +1,25 @@
 type BaseRule = {
-    id: string,
-    name: string,
-    rule_criteria: {
-        type: "linear_percentage_contribution",
-        min_contribution_threshold: number,
-        max_contribution_threshold: number,
-        min_score: number,
-        max_score: number
-    }
+  id: string;
+  name: string;
+  rule_criteria: {
+    type: "linear_percentage_contribution";
+    min_contribution_threshold: number;
+    max_contribution_threshold: number;
+    min_score: number;
+    max_score: number;
+  };
 };
 
 type EntityRule = BaseRule & {
-    entities: string[],
-    categories?: undefined,
-}
+  entities: string[];
+  categories?: undefined;
+};
 
 type CategoryRule = BaseRule & {
-    categories: string[]
-    entities?: undefined,
-}
+  categories: string[];
+  entities?: undefined;
+};
 
-type Rule = EntityRule | CategoryRule
+type Rule = EntityRule | CategoryRule;
 
-export default Rule
+export default Rule;
